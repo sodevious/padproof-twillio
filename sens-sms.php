813d46cdf6a51efaf1e25a10fc99a7bc2e2195ef
Nicole Dominguez <sodevious.net@gmail.com>
2013-11-10 18:07:00 -0500

    New numbers

--- sens-sms.php
+++ sens-sms.php
@@ -9,7 +9,7 @@ $AuthToken = "6d850ddb3b2140e73425a9c343aa69e5";
 $client = new Services_Twilio($AccountSid, $AuthToken);
  
 $sms = $client->account->sms_messages->create(
-    "646-783-1693", // From this number
+    "646-679-5723", // From this number
     "954-646-3439", // To this number
     "IT WORKED Test message!"
 );
